f39b92950a84a391194ee9cf27c0e7dd17edbc52
jaiminpatel65 <jaimpatel65@gmail.com>
2015-05-27 15:56:33 +0530

    GridView Save.

---
+++ Default.aspx
@@ -0,0 +1,107 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="State"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlState" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlState_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="City"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlCity" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlCity_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Area"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="ddlArea" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Profile Pic"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+        <asp:FileUpload ID="FileUploadProfilePic" runat="server" AllowMultiple="True" />
+        &nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lbpic" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="id" ForeColor="#333333" GridLines="None" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:CommandField HeaderText="Edit" ShowEditButton="True" />
+                <asp:CommandField HeaderText="Delete" ShowDeleteButton="True" />
+                <asp:TemplateField HeaderText="NAME">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("name") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label6" runat="server" Text='<%# Bind("name") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="STATE">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox2" runat="server" Text='<%# Bind("state") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label7" runat="server" Text='<%# Bind("state") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="CITY">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox3" runat="server" Text='<%# Bind("city") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label8" runat="server" Text='<%# Bind("city") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="AREA">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox4" runat="server" Text='<%# Bind("area") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label9" runat="server" Text='<%# Bind("area") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="ProfilePic">
+                    <EditItemTemplate>
+                        <asp:FileUpload ID="FileUploadUpdate" runat="server" AllowMultiple="True" Enabled='<%# Bind("ProfilePic") %>' />
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Image ID="Image1" runat="server" ImageUrl='<%# Bind("ProfilePic") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection conn = new SqlConnection(@"Data Source=JAIMIN-PC\JAIMIN;Initial Catalog=MyDb;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FillGridView();
+            BindDDLState();
+        }
+    }
+
+    private void BindDDLState()
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("select * from state", conn);
+        ddlState.DataSource = cmd.ExecuteReader();
+        ddlState.DataTextField = "state";
+        ddlState.DataValueField = "id";
+        ddlState.DataBind();
+        conn.Close();
+
+        ListItem li = new ListItem();
+        li.Text = "--Select--";
+        li.Value = "0";
+        ddlState.Items.Insert(0, li);
+    }
+
+    private void FillGridView()
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("select * from myreg", conn);
+        GridView1.DataSource = cmd.ExecuteReader();
+        GridView1.DataBind();
+        conn.Close();
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        string fname = FileUploadProfilePic.FileName;
+        string fextension = Path.GetExtension(fname);
+        if (FileUploadProfilePic.HasFiles)
+        {
+           // string fname = FileUploadProfilePic.FileName;
+           // string fextension = Path.GetExtension(fname);
+            if (fextension == ".jpg")
+            {
+                FileUploadProfilePic.SaveAs(Server.MapPath("Files/") + fname);
+                lbpic.Text = "File Uploaded Successfully..";
+            }
+
+            else
+            {
+                lbpic.Text = "Please Upload JPG Files Only..";
+            }
+        }
+        else
+        {
+            lbpic.Text = "Please Upload File First...";
+        }
+
+        conn.Open();
+        SqlCommand sc = new SqlCommand("insert into myreg values(@name,@state,@city,@area,@pic)", conn);
+        sc.Parameters.AddWithValue("@name", txtName.Text);
+        sc.Parameters.AddWithValue("@state", ddlState.SelectedItem.Text);
+        sc.Parameters.AddWithValue("@city", ddlCity.SelectedItem.Text);
+        sc.Parameters.AddWithValue("@area", ddlArea.SelectedItem.Text);
+        sc.Parameters.AddWithValue("@pic", FileUploadProfilePic.FileBytes);
+        sc.ExecuteNonQuery();
+        conn.Close();
+        FillGridView();
+
+
+        
+    }
+    protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("select * from city where state_id=@sid", conn);
+        cmd.Parameters.AddWithValue("@sid", ddlState.SelectedItem.Value);
+        ddlCity.DataSource = cmd.ExecuteReader();
+        ddlCity.DataTextField = "city";
+        ddlCity.DataValueField = "c_id";
+        ddlCity.DataBind();
+        conn.Close();
+
+        ListItem li = new ListItem();
+        li.Text = "--Select--";
+        li.Value = "0";
+        ddlCity.Items.Insert(0, li);
+    }
+    protected void ddlCity_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("select * from Area where c_id=@cid", conn);
+        cmd.Parameters.AddWithValue("@cid", ddlCity.SelectedItem.Value);
+        ddlArea.DataSource = cmd.ExecuteReader();
+        ddlArea.DataTextField = "area_name";
+        ddlArea.DataValueField = "a_id";
+        ddlArea.DataBind();
+        conn.Close();
+
+        ListItem li = new ListItem();
+        li.Text = "--Select--";
+        li.Value = "0";
+        ddlArea.Items.Insert(0, li);
+    }
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("delete from myreg where id=@id", conn);
+        cmd.Parameters.AddWithValue("@id", Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value));
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        FillGridView();
+    }
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        FillGridView();
+    }
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        FillGridView();
+    }
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("update myreg set name=@nm,state=@st,city=@ct,area=@ar,@pic where id=@id", conn);
+        //TextBox id = (TextBox)GridView1.Rows[e.RowIndex].Cells[2].Controls[0];
+        TextBox name = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox1");
+        TextBox state = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox2");
+        TextBox city = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox3");
+        TextBox area = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox4");
+        FileUpload fup = (FileUpload)GridView1.Rows[e.RowIndex].FindControl("FileUploadUpdate");
+       // Image im = (Image)GridView1.Rows[e.RowIndex].FindControl("Image2");
+        cmd.Parameters.AddWithValue("@id", Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value));
+        cmd.Parameters.AddWithValue("@nm", name.Text);
+        cmd.Parameters.AddWithValue("@st", state.Text);
+        cmd.Parameters.AddWithValue("@ct", city.Text);
+        cmd.Parameters.AddWithValue("@ar", area.Text);
+        cmd.Parameters.AddWithValue("@pic", fup.FileBytes);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        GridView1.EditIndex = -1;
+        FillGridView();
+    }
+}
